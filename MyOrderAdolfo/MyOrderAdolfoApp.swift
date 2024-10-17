f371dba1d70b873951d73aa85c482fb62981687c
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2024-10-17 18:26:18 -0400

    Initial Commit

---
+++ MyOrderAdolfo/MyOrderAdolfoApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyOrderAdolfoApp.swift
+//  MyOrderAdolfo
+//
+//  Created by David Romero on 2024-10-17.
+//
+
+import SwiftUI
+
+@main
+struct MyOrderAdolfoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
